2c5378a6b3ef23e172f190479853b60ede71fa1c
Mozafari1 <m_rahmat@hotmail.no>
2020-09-02 11:11:58 +0200

    Move ram out of top level

--- Selection_sort.srcs/sim_1/new/top_tb.vhd
+++ Selection_sort.srcs/sim_1/new/top_tb.vhd
@@ -1,14 +1,35 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-LIBRARY work;
+LIBRARY WORK;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
 ENTITY top_tb IS
+
 END top_tb;
 ARCHITECTURE tb OF top_tb IS
 	SIGNAL clk, rst : STD_LOGIC;
 	CONSTANT clk_period : TIME := 500 ns;
+    SIGNAL	addr_in : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL data_in :  STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL write : STD_LOGIC;
+    SIGNAL 	output :  STD_LOGIC_VECTOR(7 DOWNTO 0);
+    TYPE t_data IS ARRAY (15 DOWNTO 0) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL data : t_data := (x"0A", x"0B", x"02", x"0F", x"03", x"0A", x"07", x"09", x"01", x"04", x"07", x"15",
+	x"42", x"54", x"45", x"77");
+
 BEGIN
+PROCESS (clk)
+	BEGIN
+		IF rising_edge (clk) AND write = '1' THEN
+			data(to_integer(unsigned(addr_in(3 DOWNTO 0)))) <= data_in;
+		END IF;
+	END PROCESS;
+
+	output <= data(to_integer(unsigned(addr_in(3 DOWNTO 0))));
+	
 	uut : ENTITY work.top
-		PORT MAP(clk => clk, rst => rst);
+	
+		PORT MAP(clk => clk, rst => rst, ram_addr=> addr_in, ram_data => data_in, ram_write => write, ram_output => output);
 	clk_process : PROCESS
 	BEGIN
 		clk <= '0';
--- Selection_sort.srcs/sources_1/new/top.vhd
+++ Selection_sort.srcs/sources_1/new/top.vhd
@@ -4,18 +4,23 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY top IS
+    GENERIC (
+            length: UNSIGNED(7 DOWNTO 0) := x"10"
+            );
 	PORT (
-		clk, rst : IN STD_LOGIC
+		clk, rst : IN STD_LOGIC;
+		ram_write: OUT STD_LOGIC;
+		ram_data, ram_addr :OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+		ram_output :IN  STD_LOGIC_VECTOR(7 DOWNTO 0)
 	);
 END;
 ARCHITECTURE arch OF top IS
 	SIGNAL comp_out : STD_LOGIC;
 
-	SIGNAL min_v_load, min_i_load, index_v_load, c_cnt_load, c_cnt_incr, i_cnt_incr, write, data_mux : STD_LOGIC;
+	SIGNAL min_v_load, min_i_load, index_v_load, c_cnt_load, c_cnt_incr, i_cnt_incr, data_mux : STD_LOGIC;
 	SIGNAL addr_mux : STD_LOGIC_VECTOR(1 DOWNTO 0);
 	---------------------------------
-	SIGNAL ram_data_in, ram_addr_in : STD_LOGIC_VECTOR(7 DOWNTO 0);
-	SIGNAL ram_output : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL temp_ram_addr : STD_LOGIC_VECTOR(7 DOWNTO 0);
 	----------------------------
 	SIGNAL i_cnt_output : STD_LOGIC_VECTOR(7 DOWNTO 0);
 	-------------------------------
@@ -26,8 +31,11 @@ ARCHITECTURE arch OF top IS
 	SIGNAL reg_min_i_output : STD_LOGIC_VECTOR(7 DOWNTO 0);
 	SIGNAL reg_index_v_output : STD_LOGIC_VECTOR(7 DOWNTO 0);
 BEGIN
-
-	control : ENTITY work.control PORT MAP(
+    ram_addr <= temp_ram_addr;
+    
+	control : ENTITY work.control 
+	GENERIC MAP (length => length)
+	PORT MAP(
 		clk => clk,
 		rst => rst,
 
@@ -41,18 +49,11 @@ BEGIN
 		c_cnt_load => c_cnt_load,
 		c_cnt_incr => c_cnt_incr,
 		i_cnt_incr => i_cnt_incr,
-		write => write,
+		write => ram_write,
 		data_mux => data_mux,
 		addr_mux => addr_mux
 		);
 
-	ram : ENTITY work.ram PORT MAP(
-		clk => clk,
-		data_in => ram_data_in,
-		addr_in => ram_addr_in,
-		output => ram_output,
-		write => write
-		);
 
 	i_cnt : ENTITY work.counter PORT MAP(
 		input => (OTHERS => '0'),
@@ -82,7 +83,7 @@ BEGIN
 	reg_min_i : ENTITY work.reg PORT MAP(
 		clk => clk,
 		rst => rst,
-		input => ram_addr_in,
+		input => temp_ram_addr,
 		load => min_i_load,
 		output => reg_min_i_output
 		);
@@ -99,12 +100,12 @@ BEGIN
 		B => i_cnt_output,
 		C => reg_min_i_output,
 		ctr => addr_mux,
-		output => ram_addr_in
+		output => temp_ram_addr
 		);
 	data_mux_en : ENTITY work.data_mux PORT MAP(
 		A => reg_index_v_output,
 		B => reg_min_v_output,
-		output => ram_data_in,
+		output => ram_data,
 		ctr => data_mux
 		);
 	comparator : ENTITY work.comparator PORT MAP(
