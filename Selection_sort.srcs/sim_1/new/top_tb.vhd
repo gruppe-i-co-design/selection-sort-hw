84b01a7208ef396c87f7ca967253c0a05bf2af26
Ole Martin Ruud <dev@barskern.no>
2020-09-02 10:14:41 +0200

    Format simulation source

--- Selection_sort.srcs/sim_1/new/top_tb.vhd
+++ Selection_sort.srcs/sim_1/new/top_tb.vhd
@@ -1,27 +1,27 @@
-library ieee;
-use ieee.std_logic_1164.all;
-library work;
-entity top_tb is
-end top_tb;
-architecture tb of top_tb is
-    signal clk, rst : std_logic;
-    constant clk_period : time := 500 ns;
-begin
-	uut : entity work.top
-    port map (clk => clk, rst => rst);
-clk_process: process 
-   begin
-      clk <= '0';
-      wait for clk_period/2;
-      clk <= '1';
-      wait for clk_period/2;
-   end process;
--- Stimuli process 
-   stim_proc: process
-      begin
-         rst <= '1';      
-         wait for clk_period*2;
-         rst <= '0';      
-         wait for clk_period*1000;
-      end process ;
-end tb;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+LIBRARY work;
+ENTITY top_tb IS
+END top_tb;
+ARCHITECTURE tb OF top_tb IS
+	SIGNAL clk, rst : STD_LOGIC;
+	CONSTANT clk_period : TIME := 500 ns;
+BEGIN
+	uut : ENTITY work.top
+		PORT MAP(clk => clk, rst => rst);
+	clk_process : PROCESS
+	BEGIN
+		clk <= '0';
+		WAIT FOR clk_period/2;
+		clk <= '1';
+		WAIT FOR clk_period/2;
+	END PROCESS;
+	-- Stimuli process 
+	stim_proc : PROCESS
+	BEGIN
+		rst <= '1';
+		WAIT FOR clk_period * 2;
+		rst <= '0';
+		WAIT FOR clk_period * 1000;
+	END PROCESS;
+END tb;
